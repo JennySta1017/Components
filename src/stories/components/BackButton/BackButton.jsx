db3e6e11281a3f2780e19f1253fe5fbc56d2f40d
Marvin Barrett <marvin.barrett@mb@gmail.com>
2024-09-19 16:04:56 +0200

    Lade till beskrivande text till BackButton

--- src/stories/components/BackButton/BackButton.jsx
+++ src/stories/components/BackButton/BackButton.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
-import styles from './BackButton.module.css'; 
+import { useNavigate } from 'react-router-dom';
 
-const BackButton = ({ onClick }) => {
-    return (
-        <button className={styles.backButton} onClick={onClick}>
-            Tillbaka
-        </button>
-    );
-}
+const BackButton = () => {
+  const navigate = useNavigate();
+
+  const handleBackClick = () => {
+    navigate(-1);
+  };
+
+  return (
+    <div>
+      <h2>Gå tillbaka</h2>
+      <p>Den här knappen tar dig tillbaka till den tidigare sidan du var på. 😃</p>
+      <button onClick={handleBackClick}>
+        Tillbaka
+      </button>
+    </div>
+  );
+};
 
 export default BackButton;
--- src/stories/components/BackButton/BackButton.stories.jsx
+++ src/stories/components/BackButton/BackButton.stories.jsx
@@ -1,10 +1,20 @@
-// BackButton.stories.jsx
 import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
 import BackButton from './BackButton';
 
 export default {
-    title: 'TE/BackButton',
-    component: BackButton,
+  title: 'TE/BackButton',
+  component: BackButton,
+  decorators: [
+    (Story) => (
+      <MemoryRouter>
+        <Story />
+      </MemoryRouter>
+    ),
+  ],
 };
 
-export const Default = () => <BackButton onClick={() => alert('Going back!')} />;
+const Template = (args) => <BackButton {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {};
