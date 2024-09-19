79fe7421e558be1e4440a96b1fe70ab8329bee49
Marvin Barrett <marvin.barrett@mb@gmail.com>
2024-09-19 16:04:56 +0200

    lite html till sortiment

--- src/stories/components/sortiment/ListTe-sort.jsx
+++ src/stories/components/sortiment/ListTe-sort.jsx
@@ -1,9 +1,28 @@
 import React from "react";
 
 const ListTe = () => {
-    return(
-        <>Te lista</>
-    )
+    const Tesortiment = //Tillfällig
+        {"Svart te":["Earl Grey", "Chai", "Lichi"],
+         "Grönt te":["Jasmine", "Matcha", "Yogi", "Longjin"],
+         "Rött te":["Roiboos", "Skogsglänta Roiboos"], 
+         "Vitt te":["White Cassis","Jasmine silver needle"]
+        };
+
+        return (
+        <div>
+            <h2>Tesortiment</h2>
+            {Object.keys(Tesortiment).map((kategori, index) => (
+                <div key={index}>
+                <h3>{kategori}</h3>
+                <ul>
+                    {Tesortiment[kategori].map((te, i) => (
+                        <li key={i}>{te}</li>
+                    ))}
+                    </ul>
+                </div>
+             ))}
+       </div>
+    );
 }
 
 
