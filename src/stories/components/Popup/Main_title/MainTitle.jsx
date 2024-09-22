90d550e79c100a9566505bf8a67de84683b4cc72
Selma Brattli <selma@brattli.nu>
2024-09-22 15:39:27 +0200

    added title in popup and added font in index.html

---
+++ src/stories/components/Popup/Main_title/MainTitle.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from './MainTitle.module.css'
+
+const MainTitle = () => {
+    return(
+        <div>
+            <label className={styles.title}>
+                Title
+            </label>
+        </div>
+    )
+}
+
+export default MainTitle
---
+++ src/stories/components/Popup/Main_title/MainTitle.stories.jsx
@@ -0,0 +1,10 @@
+import MainTitle from "./MainTitle";
+
+export default {
+    title: 'TE/Popup/MainTitle',
+    component: MainTitle,
+}
+
+export const Default = {
+    
+}
