919aceba9711135e27009207a8d6c789e1db5151
Marvin Barrett <marvin.barrett@mb@gmail.com>
2024-09-24 12:52:43 +0200

    skapat komponent för tesortiment, lagt till enkel css

--- src/stories/components/sortiment/ListTe-sort.jsx
+++ src/stories/components/sortiment/ListTe-sort.jsx
@@ -1,29 +1,27 @@
 import React from "react";
+import styles from "./ListTe-sort.module.css"; 
 
-const ListTe = () => {
-    const Tesortiment = //Tillfällig
-        {"Svart te":["Earl Grey", "Chai", "Lichi"],
-         "Grönt te":["Jasmine", "Matcha", "Yogi", "Longjin"],
-         "Rött te":["Roiboos", "Skogsglänta Roiboos"], 
-         "Vitt te":["White Cassis","Jasmine silver needle"]
-        };
-
-        return (
-        <div>
-            <h2>Tesortiment</h2>
-            {Object.keys(Tesortiment).map((kategori, index) => (
-                <div key={index}>
-                <h3>{kategori}</h3>
-                <ul>
-                    {Tesortiment[kategori].map((te, i) => (
-                        <li key={i}>{te}</li>
-                    ))}
-                    </ul>
-                </div>
-             ))}
-       </div>
+const ListTe = ({ teas = [] }) => { 
+    return (
+        <div className={styles.container}>
+            <h2 className={styles.header}>Alla te sorter</h2>
+            <div className={styles.teaGrid}>
+                {teas.map(category => (
+                    category.products.map((te, index) => (
+                        <div className={styles.teaItem} key={index}>
+                            <h4>{te.name}</h4>
+                            <img
+                                src={te.image}
+                                alt={te.name}
+                                className={styles.teaImage}
+                            />
+                        </div>
+                    ))
+                ))}
+            </div>
+        </div>
     );
-}
-
+};
 
 export default ListTe;
+
