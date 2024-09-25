36d637d22c7c6fdeac4e187539cfb3dc0f1437b1
Selma Brattli <selma@brattli.nu>
2024-09-25 14:49:27 +0200

    fixed index.js imports

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import {BackButton} from "./stories/components/BackButton";
-import { ContactInfo } from "./stories/components/Contact";
-import { ContactLink } from "./stories/components/Contact";
+import { ContactInfo } from "./stories/components/Contact/ContactInfo";
+import { ContactLink } from "./stories/components/Contact/Contaktlink";
 import { Filter } from "./stories/components/Filter";
 import { Footer } from "./stories/components/Footer";
 import { ShowImages } from "./stories/components/Images";
 import { Popup } from "./stories/components/Popup/version_1";
-import { Details } from "./stories/components/Popup/version_2";
-import { SearchBar } from "./stories/components/Searchbar";
+import { Details } from "./stories/components/Popup/version_2/details";
+import { SearchBar } from "./stories/components/Searchbar/Searchbar";
 import { SearchResult } from "./stories/components/SearchResult";
 import { FetchTe } from "./stories/components/sortiment";
 
