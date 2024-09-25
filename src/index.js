355bb78c450298ca5b97e695c508d2c5fe695297
Jenny Stark <jenny.stark1017@gmail.com>
2024-09-25 14:08:07 +0200

    index klar för Rollup konfig

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import {BackButton} from "./stories/components/BackButton";
+import { ContactInfo } from "./stories/components/Contact";
+import { ContactLink } from "./stories/components/Contact";
+import { Filter } from "./stories/components/Filter";
+import { Footer } from "./stories/components/Footer";
+import { ShowImages } from "./stories/components/Images";
+import { Popup } from "./stories/components/Popup/version_1";
+import { Details } from "./stories/components/Popup/version_2";
+import { SearchBar } from "./stories/components/Searchbar";
+import { SearchResult } from "./stories/components/SearchResult";
+import { FetchTe } from "./stories/components/sortiment";
+
+export {BackButton, ContactInfo, ContactLink, Filter, Footer, ShowImages, Popup, Details, SearchBar, SearchResult, FetchTe};
