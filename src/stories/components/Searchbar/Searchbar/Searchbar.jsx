d1fb49d4e066d8795ac9a847dac747050db1cde1
Jenny Stark <jenny.stark1017@gmail.com>
2024-10-02 16:57:20 +0200

    Funktion serachbar klar

--- src/stories/components/Searchbar/Searchbar/Searchbar.jsx
+++ src/stories/components/Searchbar/Searchbar/Searchbar.jsx
@@ -4,33 +4,75 @@ import InputField from "../Inputfield/Inputfield";
 import SearchResult from "../../SearchResult/SearchResult";
 
 
-
+// teData kommer att läggas in som en prop i SearchBar i stället för att ha datan här
 const SearchBar = () => {   
 
   const teaData = [
-    { id: 1, name: "Grönt te", image: "https://..." },
-    { id: 2, name: "Svart te", image: "https://..." },
-    { id: 3, name: "Rött te", image: "https://..." },
+    {
+      id: 1,
+      name: "Grönt te",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHfamFahLTBWDTLUKhzs6wYtrsZd5HHjmTtA&s",
+    },
+    {
+      id: 2,
+      name: "Svart te",
+      image:
+        "https://www.healthifyme.com/blog/wp-content/uploads/2020/02/Black-Tea-2-1.jpg",
+    },
+    {
+      id: 3,
+      name: "Rött te",
+      image: "https://kahlstkh.se/wp-content/uploads/2018/03/te-rooibos.jpg",
+    },
+    {
+      id: 4,
+      name: "Vitt te",
+      image:
+        "https://sakiproducts.com/cdn/shop/articles/20221107133813-white-tea-recipe-blog_800x800.jpg?v=1667828741",
+    },
+    {
+      id: 5,
+      name: "Jasmin silver needle",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxmRhyN1GjGMoEycRZfpb9b4ahFBlH_H9Vrg",
+    },
   ];
+  
 
   const [searchInput, setSearchInput] = useState('');
+  const [filteredTeas, setFilteredTeas] = useState([]);
   
   const searchHandeler = (e) => {
     setSearchInput(e.target.value);
 
 }
 
-// Filtrera te baserat på sökordet
-const filteredTeas = teaData.filter((tea) => {
-  const searchWords = searchInput.toLowerCase().split(" ");
-  const teaWords = tea.name.toLowerCase().split(" ");
-  
-  return searchWords.some(searchWord => 
-    teaWords.some(teaWord => teaWord.includes(searchWord))
-    
-  );
-  
-});
+const handleSearchClick = () => {
+   // Kontrollera om sökfältet är tomt
+   if (!searchInput) {   
+    alert('Sök på en tesort');   
+    return;
+  }
+  // Filtrera te baserat på sökordet
+  const filtered = teaData.filter((tea) => {
+    const searchWords = searchInput.toLowerCase().split(" ");
+    const teaWords = tea.name.toLowerCase().split(" ");
+    return searchWords.some((searchWord) =>
+      teaWords.some((teaWord) => teaWord.includes(searchWord))
+    );
+  });
+
+  // Om inga resultat hittades
+  if (filtered.length === 0) {
+    alert('Inga tesorter matchade din sökning');
+    return;
+  }
+
+  setFilteredTeas(filtered);
+  // Rensa sökfältet
+  setSearchInput('');
+};
 
 
     return (
@@ -46,7 +88,7 @@ const filteredTeas = teaData.filter((tea) => {
       </ul>
     </div>
           <InputField searchHandeler={searchHandeler}/> 
-          <SearchButton /> 
+          <SearchButton handleSearchClick={handleSearchClick}/> 
           <SearchResult />
         </div>
 
