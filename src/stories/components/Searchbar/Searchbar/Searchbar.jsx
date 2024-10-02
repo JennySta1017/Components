dc4cdfd7cfb94ee1751c5fd058e1804c0f241deb
Jenny Stark <jenny.stark1017@gmail.com>
2024-10-02 14:19:23 +0200

    funktion för searchbar

--- src/stories/components/Searchbar/Searchbar/Searchbar.jsx
+++ src/stories/components/Searchbar/Searchbar/Searchbar.jsx
@@ -1,10 +1,17 @@
 import React, {useState} from "react";
 import SearchButton from "../Button/Button";
 import InputField from "../Inputfield/Inputfield";
+import SearchResult from "../../SearchResult/SearchResult";
 
 
 
 const SearchBar = () => {   
+
+  const teaData = [
+    { id: 1, name: "Grönt te", image: "https://..." },
+    { id: 2, name: "Svart te", image: "https://..." },
+    { id: 3, name: "Rött te", image: "https://..." },
+  ];
 
   const [searchInput, setSearchInput] = useState('');
   
@@ -13,10 +20,34 @@ const SearchBar = () => {
 
 }
 
+// Filtrera te baserat på sökordet
+const filteredTeas = teaData.filter((tea) => {
+  const searchWords = searchInput.toLowerCase().split(" ");
+  const teaWords = tea.name.toLowerCase().split(" ");
+  
+  return searchWords.some(searchWord => 
+    teaWords.some(teaWord => teaWord.includes(searchWord))
+    
+  );
+  
+});
+
+
     return (
         <div>
+          <div>
+      <ul>
+        {filteredTeas.map((tea) => (
+          <li key={tea.id}>
+            <img src={tea.image} alt={tea.name} width="50" />
+            <p>{tea.name}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
           <InputField searchHandeler={searchHandeler}/> 
           <SearchButton /> 
+          <SearchResult />
         </div>
 
     )
