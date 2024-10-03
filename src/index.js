cecb35b73da2eda444aed8e7163ce0728a825507
Jenny Stark <jenny.stark1017@gmail.com>
2024-10-03 19:37:07 +0200

    ready to build

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 //import {BackButton} from "./stories/components/BackButton";
-import {Header} from "./stories/components/Header";
 import { ContactInfo } from "./stories/components/Contact/ContactInfo";
 import { Filter } from "./stories/components/Filter";
 import { Footer } from "./stories/components/Footer";
@@ -8,6 +7,7 @@ import { Popup } from "./stories/components/Popup/version_1";
 import { SearchBar } from "./stories/components/Searchbar/Searchbar";
 import { SearchResult } from "./stories/components/SearchResult";
 import { FetchTe } from "./stories/components/sortiment";
+import { Header } from "./stories/components/Header";
 
 
-export {Header, ContactInfo, Filter, Footer, ShowImages, Popup, SearchBar, SearchResult, FetchTe};
+export { ContactInfo, Filter, Footer, ShowImages, Popup, SearchBar, SearchResult, FetchTe, Header };
