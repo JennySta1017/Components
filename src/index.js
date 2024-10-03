d3e0a89a3fbe04023c2824921046121137b981af
Jenny Stark <jenny.stark1017@gmail.com>
2024-10-03 17:20:21 +0200

    exporting Header in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 //import {BackButton} from "./stories/components/BackButton";
+import {Header} from "./stories/components/Header";
 import { ContactInfo } from "./stories/components/Contact/ContactInfo";
-import { ContactLink } from "./stories/components/Contact/Contaktlink";
 import { Filter } from "./stories/components/Filter";
 import { Footer } from "./stories/components/Footer";
 import { ShowImages } from "./stories/components/Images";
@@ -8,6 +8,6 @@ import { Popup } from "./stories/components/Popup/version_1";
 import { SearchBar } from "./stories/components/Searchbar/Searchbar";
 import { SearchResult } from "./stories/components/SearchResult";
 import { FetchTe } from "./stories/components/sortiment";
-import {Header} from "./stories/components/Header";
 
-export {Header, ContactInfo, ContactLink, Filter, Footer, ShowImages, Popup, SearchBar, SearchResult, FetchTe};
+
+export {Header, ContactInfo, Filter, Footer, ShowImages, Popup, SearchBar, SearchResult, FetchTe};
