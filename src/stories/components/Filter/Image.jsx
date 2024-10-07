ff28c1ec3a553b6c9c67c0dd05073b237ec9089a
Selma Brattli <selma@brattli.nu>
2024-10-07 14:26:52 +0200

    pass if no props are passed in image.jsx

--- src/stories/components/Filter/Image.jsx
+++ src/stories/components/Filter/Image.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import styles from "./Image.module.css";
 
 const Image = ({ image, name }) => {
+
+  if(!image || !name) {
+    return null;
+  }
+
   return (
     <div className={styles.container}>
       <img className={styles.image} src={image} alt={name} />
